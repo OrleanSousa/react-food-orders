5760857d1683a9571f333b42e0fcf9cad169d126
Orlean Sousa <orleansilva60@hotmail.com>
2024-09-20 17:06:32 -0300

    mi frist commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState();
+
+  function showCartHandler() {
+    setCartIsShown(true);
+  }
+  function hideCartHandler() {
+    setCartIsShown(false);
+  }
+  return (
+    <CartProvider>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
